1b85c76a0ccc7679ff0744da07cfd22cb9ab9a8d
Your Name <you@example.com>
2022-02-04 13:57:35 +0100

    home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TvShowRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(TvShowRepository $tvShowRepository): Response
+    {
+        $latestTvshows = $tvShowRepository->findBy([], ['id' => 'DESC'], 3);
+
+        dd($latestTvshows);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
